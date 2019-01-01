99f4cb76a013a419e7e937a49919560c1bf7236a
Simon Pedersen <blasimtex@hotmail.com>
2019-01-01 23:02:35 +0100

    Add project files.

---
+++ SJKP.ARMTools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SJKP.ARMTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
